b9cb9c651df5583a3f7e0b98783c29cda57c0263
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2021-03-14 15:59:47 +0000

    added tech stack collection type

--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -8,8 +8,9 @@
 module.exports = {
     async create(ctx) {
         let entity;
-        let sdgCategoryArray = await strapi.query('sdg-category').find({ name_in: ctx.request.body.sdg_categories });
-        let projectCategoryArray = await strapi.query('project-category').find({ name_in: ctx.request.body.project_categories })
+        console.log("THe body ",ctx.request.body)
+        let sdgCategoryArray = await strapi.query('sdg-category').find({ name_in: ctx.request.body.sdg_categories ? ctx.request.body.sdg_categories : [] });
+        let projectCategoryArray = await strapi.query('project-category').find({ name_in: ctx.request.body.project_categories ? ctx.request.body.project_categories : [] })
 
         let body = ctx.request.body
         body.sdg_categories = sdgCategoryArray
