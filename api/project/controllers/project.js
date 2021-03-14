b1da4655dea70ed0bf8ac3654d91e54ebc607e6e
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2021-03-14 08:32:46 +0000

    rebuilt project create controller

--- api/project/controllers/project.js
+++ api/project/controllers/project.js
@@ -5,4 +5,22 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        let entity;
+        let sdgCategoryArray = await strapi.query('sdg-category').find({ name_in: ctx.request.body.sdg_categories });
+        let projectCategoryArray = await strapi.query('project-category').find({ name_in: ctx.request.body.project_categories })
+
+        let body = ctx.request.body
+        body.sdg_categories = sdgCategoryArray
+        body.project_categories = projectCategoryArray
+
+        if (ctx.is('multipart')) {
+          //const { data, files } = parseMultipartData(ctx);
+          //entity = await strapi.services.restaurant.create(data, { files });
+        } else {
+          entity = await strapi.services.project.create(body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.project });
+      },
+};
