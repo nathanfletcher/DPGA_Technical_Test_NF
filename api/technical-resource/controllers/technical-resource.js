0ceb89b644745a20ea055ffde7fb4732d9bfd004
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2021-03-14 17:12:50 +0000

    creating tech resource entities from Google Forms

--- api/technical-resource/controllers/technical-resource.js
+++ api/technical-resource/controllers/technical-resource.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        let entity;
+        console.log("THe body ",ctx.request.body)
+        let technologyStackArray = await strapi.query('technology-stack').find({ name_in: ctx.request.body.technology_stacks ? ctx.request.body.technology_stacks : [] });
+
+        let body = ctx.request.body
+        body.technology_stacks = technologyStackArray
+
+        if (ctx.is('multipart')) {
+          //const { data, files } = parseMultipartData(ctx);
+          //entity = await strapi.services.restaurant.create(data, { files });
+        } else {
+          entity = await strapi.services["technical-resource"].create(body);
+        }
+        return entity;
+      },
+};
